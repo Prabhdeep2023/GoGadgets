29c15bdf972f9f5ce065cd8a692989493fde6518
Prabhdeep Singh <prsi744@ess.ais.ac.nz>
2023-07-10 01:40:23 +1200

    first commit

---
+++ config/database.js
@@ -0,0 +1,46 @@
+// database module
+var mysql = require('mysql2');
+
+// development server
+// var config = {
+//    host: 'localhost',
+//    user: 'root',
+//    password: 'PD87@mysql',
+//    database: 'aStore'
+// };
+
+// production server
+var config = {
+    host: 'ggdemoserver2.mysql.database.azure.com',
+    user: 'dbuser',
+    password: 'PD87@mysql',
+    database: 'astore'
+};
+
+// init database
+var pool = mysql.createPool(config);
+
+//Fetch data
+function RunQuery(sql, callback) {
+    pool.getConnection(function (err, conn) {
+        if (err) {
+            ShowErrors(err);
+        }
+        conn.query(sql, function (err, rows, fields) {
+            if (err) {
+                ShowErrors(err);
+            }
+            conn.release();
+            callback(rows);
+        });
+    });
+}
+
+//Throw errors
+function ShowErrors(err) {
+    throw err;
+}
+
+module.exports = {
+    RunQuery: RunQuery
+};
